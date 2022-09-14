92460eb51d19ae0d55ce7bcb52e5e4927856ccd3
Sam Chan <samchan0221@gmail.com>
2022-09-14 17:56:34 +0800

    Setup minimal AP_PaySDK crashing demo

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'debug-asiapay-sdk' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for debug-asiapay-sdk
+  pod 'AP_PaySDK', '2.6.18'
+
+  target 'debug-asiapay-sdkTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'debug-asiapay-sdkUITests' do
+    # Pods for testing
+  end
+
+end
--- debug-asiapay-sdk/ViewController.m
+++ debug-asiapay-sdk/ViewController.m
@@ -6,6 +6,12 @@
 //
 
 #import "ViewController.h"
+#import <AP_PaySDK/AP_PaySDK.h>
+#import <AP_PaySDK/AP_PaySDK-Swift.h>
+#import <PassKit/PassKit.h>
+
+@import AP_PaySDK;
+
 
 @interface ViewController ()
 
@@ -15,8 +21,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    PaySDK *paySDK = [PaySDK shared];
     // Do any additional setup after loading the view.
-}
+    paySDK.paymentDetails = [[PayData alloc] initWithChannelType: PayChannelDIRECT
+                                                         envType: EnvTypeSANDBOX
+                                                          amount: @"8499.0"
+                                                          payGate: PayGatePAYDOLLAR
+                                                          currCode: CurrencyCodeHKD
+                                                          payType: payTypeNORMAL_PAYMENT
+                                                          orderRef: @"3_4cf6e22e_010053795"
+                                                          payMethod: @"ALIPAYHKAPP"
+                                                          lang: LanguageENGLISH
+                                                          merchantId: @"YOUR_MERCHANT_ID"
+                                                          remark: @""
+                                                          payRef: @""
+                                                          resultpage: nil
+                                                 showCloseButton:false
+                                                      showToolbar:false
+                                               webViewClosePrompt:@""
+                                                        extraData:nil
+                                         merchantCapabilitiesData:nil
+                                            supportedNetworksData:nil
+                             ];
+    paySDK.delegate = self;
+    [paySDK process];
+  }
 
+  - (void)paymentResultWithResult:(PayResult * _Nonnull)result {
+    NSLog(@"RCTAsiaPayModule#paymentResultWithResult");
+  }
 
+  - (void)payMethodOptionsWithMethod:(PaymentOptionsDetail * _Nonnull)method {
+    NSLog(@"RCTAsiaPayModule#payMethodOptionsWithMethod");
+  }
+
+  - (void)transQueryResultsWithResult:(TransQueryResults * _Nonnull)result {
+    NSLog(@"RCTAsiaPayModule#transQueryResultsWithResult");
+  }
+
+  - (void)showProgress {
+    NSLog(@"RCTAsiaPayModule#showProgress");
+  }
+
+  - (void)hideProgress {
+    NSLog(@"RCTAsiaPayModule#hideProgress");
+  }
 @end
